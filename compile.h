186377978b172f1e05917f0e6d14cea58d1a1f3b
dobkeratops <dobkeratopsgit@gmail.com>
2014-12-15 11:26:59 +0000

    more rearranging. committing to code arranged more sorted by type (with parse & codegen seperate)

--- compile.h
+++ compile.h
@@ -6,10 +6,3 @@ void output_code(FILE* ofp, Scope* scope, int depth);
 void name_mangle(char* dst, int size, const ExprStructDef* src);
 void name_mangle(char* dst, int size, const ExprFnDef* src);
 inline void dbg_mangle(const char*,...){}
-#if DEBUG>=2
-#define dbg_vcall dbprintf
-#else
-inline void dbg_vcall(const char*,...){}
-#endif
-
-
